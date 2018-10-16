0db355a156d4904c8790adce59b2e57f64866248
Timofey Privalov <timofmax@gmail.com>
2018-10-16 09:53:33 +0300

    write to file instead of printing to console

--- mac_lookup_vendor.py
+++ mac_lookup_vendor.py
@@ -1,6 +1,7 @@
 import urllib.request as urllib2
 import json
 import codecs
+import yaml
 from time import sleep
 
 '''
@@ -12,22 +13,30 @@ url = "http://macvendors.co/api/"
 
 
 if __name__ == '__main__':
+    #sps - is dict will use for YAML
+    sps = {}
     with open('mac_list.txt') as f:
         txt = f.read()
         mac_list = []
         for mac in txt.split('\n'):
             if mac:
                 mac_list.append(mac.strip())
-                
+
     for i in mac_list:
-        #sleep(1)
+        print("Number of MAC's: {}".format(len(mac_list)))
         request = urllib2.Request(url+i, headers={'User-Agent' : "API Browser"})
         response = urllib2.urlopen( request )
     #Fix: json object must be str, not 'bytes'
         reader = codecs.getreader("utf-8")
         obj = json.load(reader(response))
-    
+
     #Print company name
-        print (obj['result']['company']);
-        print(i+'\n')
+        vendor = (obj['result']['company'])
+        mac_addr = i
+        sps.setdefault(mac_addr, vendor)
+      
+
+print("Length of dics is: {}".format(len(sps)))
+with open("yaml_mac.txt, "w") as f:
+    yaml.dump(sps, f, default_flow_style=False)
 

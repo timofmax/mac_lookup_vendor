847ab5e20670229e6d8de5d192ad73c5c3c7ad8c
Timofey Privalov <timofmax@gmail.com>
2018-10-16 09:57:16 +0300

    Update mac_lookup_vendor.py

--- mac_lookup_vendor.py
+++ mac_lookup_vendor.py
@@ -2,7 +2,6 @@ import urllib.request as urllib2
 import json
 import codecs
 import yaml
-from time import sleep
 
 '''
 time.sleep only for occasion if macvendors API not able to handle
@@ -13,7 +12,6 @@ url = "http://macvendors.co/api/"
 
 
 if __name__ == '__main__':
-    #sps - is dict will use for YAML
     sps = {}
     with open('mac_list.txt') as f:
         txt = f.read()
@@ -21,9 +19,8 @@ if __name__ == '__main__':
         for mac in txt.split('\n'):
             if mac:
                 mac_list.append(mac.strip())
-
+    print("Number of MAC's: {}".format(len(mac_list)))
     for i in mac_list:
-        print("Number of MAC's: {}".format(len(mac_list)))
         request = urllib2.Request(url+i, headers={'User-Agent' : "API Browser"})
         response = urllib2.urlopen( request )
     #Fix: json object must be str, not 'bytes'
@@ -34,7 +31,6 @@ if __name__ == '__main__':
         vendor = (obj['result']['company'])
         mac_addr = i
         sps.setdefault(mac_addr, vendor)
-      
 
 print("Length of dics is: {}".format(len(sps)))
 with open("yaml_mac.txt", "w") as f:
